6cb0dac260ce5a44c9b0eecee7dd70a6da0e3a23
Eli Sachs <easachs13@gmail.com>
2022-07-31 23:51:57 -0600

    Extension specs

--- spec/features/supermarkets/items/index_spec.rb
+++ spec/features/supermarkets/items/index_spec.rb
@@ -3,7 +3,6 @@ require 'rails_helper'
 RSpec.describe 'Supermarket Items Index' do
   it 'has unique list of supermarkets items' do
     king = Supermarket.create!(name: "King Soops", location: "Denver")
-    safeway = Supermarket.create!(name: "Safeway", location: "Lakewood")
 
     toothpaste = Item.create!(name: "Toothpaste", price: 1)
     bread = Item.create!(name: "Bread", price: 2)
@@ -32,5 +31,58 @@ RSpec.describe 'Supermarket Items Index' do
     expect(page).to have_content("Pizza")
     expect(page).to_not have_content("Berries")
     expect(page).to_not have_content("Guac")
+  end
+
+  it 'has three most popular items based on customer associations' do
+    king = Supermarket.create!(name: "King Soops", location: "Denver")
+
+    toothpaste = Item.create!(name: "Toothpaste", price: 1) #0
+    bread = Item.create!(name: "Bread", price: 2) #2
+    bagels = Item.create!(name: "Bagels", price: 3) #1
+    pizza = Item.create!(name: "Pizza", price: 4) #5 !
+    berries = Item.create!(name: "Berries", price: 5) #3 !
+    guac = Item.create!(name: "Guac", price: 6) #4 !
+
+    ed = king.customers.create!(name: "Ed")
+    CustomerItem.create!(customer_id: ed.id, item_id: pizza.id)
+    CustomerItem.create!(customer_id: ed.id, item_id: bread.id)
+    CustomerItem.create!(customer_id: ed.id, item_id: bagels.id)
+
+    edd = king.customers.create!(name: "Edd")
+    CustomerItem.create!(customer_id: edd.id, item_id: pizza.id)
+    CustomerItem.create!(customer_id: edd.id, item_id: bread.id)
+    CustomerItem.create!(customer_id: edd.id, item_id: guac.id)
+
+    eddy = king.customers.create!(name: "Eddy")
+    CustomerItem.create!(customer_id: eddy.id, item_id: pizza.id)
+    CustomerItem.create!(customer_id: eddy.id, item_id: guac.id)
+    CustomerItem.create!(customer_id: eddy.id, item_id: berries.id)
+
+    bill = king.customers.create!(name: "Bill")
+    CustomerItem.create!(customer_id: bill.id, item_id: pizza.id)
+    CustomerItem.create!(customer_id: bill.id, item_id: guac.id)
+    CustomerItem.create!(customer_id: bill.id, item_id: berries.id)
+
+    ted = king.customers.create!(name: "Ted")
+    CustomerItem.create!(customer_id: ted.id, item_id: pizza.id)
+    CustomerItem.create!(customer_id: ted.id, item_id: guac.id)
+    CustomerItem.create!(customer_id: ted.id, item_id: berries.id)
+
+    visit "/supermarkets/#{king.id}"
+
+    within "#most-popular" do
+      within "#item-0" do
+        expect(page).to have_content("Pizza")
+      end
+      within "#item-1" do
+        expect(page).to have_content("Guac")
+      end
+      within "#item-2" do
+        expect(page).to have_content("Berries")
+      end  
+    end
+    expect(page).to_not have_content("Bagels")
+    expect(page).to_not have_content("Bread")
+    expect(page).to_not have_content("Toothpaste")
   end
 end
--- spec/models/supermarket_spec.rb
+++ spec/models/supermarket_spec.rb
@@ -4,4 +4,75 @@ RSpec.describe Supermarket, type: :model do
   describe 'relationships' do
     it { should have_many :customers }
   end
+
+  describe 'instance methods' do
+    it 'has distinct_items' do
+      king = Supermarket.create!(name: "King Soops", location: "Denver")
+
+      toothpaste = Item.create!(name: "Toothpaste", price: 1)
+      bread = Item.create!(name: "Bread", price: 2)
+      bagels = Item.create!(name: "Bagels", price: 3)
+      pizza = Item.create!(name: "Pizza", price: 4)
+      berries = Item.create!(name: "Berries", price: 5)
+      guac = Item.create!(name: "Guac", price: 6)
+
+      ed = king.customers.create!(name: "Ed")
+      CustomerItem.create!(customer_id: ed.id, item_id: toothpaste.id)
+      CustomerItem.create!(customer_id: ed.id, item_id: bread.id)
+      CustomerItem.create!(customer_id: ed.id, item_id: bagels.id)
+
+      edd = king.customers.create!(name: "Edd")
+      CustomerItem.create!(customer_id: edd.id, item_id: toothpaste.id)
+      CustomerItem.create!(customer_id: edd.id, item_id: pizza.id)
+
+      eddy = king.customers.create!(name: "Eddy")
+      CustomerItem.create!(customer_id: eddy.id, item_id: toothpaste.id)
+
+      expect(king.distinct_items).to include(toothpaste)
+      expect(king.distinct_items).to include(bread)
+      expect(king.distinct_items).to include(bagels)
+      expect(king.distinct_items).to include(pizza)
+      expect(king.distinct_items).to_not include(berries)
+      expect(king.distinct_items).to_not include(guac)
+    end
+
+    it 'has most_popular items' do
+      king = Supermarket.create!(name: "King Soops", location: "Denver")
+
+      toothpaste = Item.create!(name: "Toothpaste", price: 1) #0
+      bread = Item.create!(name: "Bread", price: 2) #2
+      bagels = Item.create!(name: "Bagels", price: 3) #1
+      pizza = Item.create!(name: "Pizza", price: 4) #5 !
+      berries = Item.create!(name: "Berries", price: 5) #3 !
+      guac = Item.create!(name: "Guac", price: 6) #4 !
+
+      ed = king.customers.create!(name: "Ed")
+      CustomerItem.create!(customer_id: ed.id, item_id: pizza.id)
+      CustomerItem.create!(customer_id: ed.id, item_id: bread.id)
+      CustomerItem.create!(customer_id: ed.id, item_id: bagels.id)
+
+      edd = king.customers.create!(name: "Edd")
+      CustomerItem.create!(customer_id: edd.id, item_id: pizza.id)
+      CustomerItem.create!(customer_id: edd.id, item_id: bread.id)
+      CustomerItem.create!(customer_id: edd.id, item_id: guac.id)
+
+      eddy = king.customers.create!(name: "Eddy")
+      CustomerItem.create!(customer_id: eddy.id, item_id: pizza.id)
+      CustomerItem.create!(customer_id: eddy.id, item_id: guac.id)
+      CustomerItem.create!(customer_id: eddy.id, item_id: berries.id)
+
+      bill = king.customers.create!(name: "Bill")
+      CustomerItem.create!(customer_id: bill.id, item_id: pizza.id)
+      CustomerItem.create!(customer_id: bill.id, item_id: guac.id)
+      CustomerItem.create!(customer_id: bill.id, item_id: berries.id)
+
+      ted = king.customers.create!(name: "Ted")
+      CustomerItem.create!(customer_id: ted.id, item_id: pizza.id)
+      CustomerItem.create!(customer_id: ted.id, item_id: guac.id)
+      CustomerItem.create!(customer_id: ted.id, item_id: berries.id)
+
+      expect(king.most_popular.length).to eq(3)
+      expect(king.most_popular).to eq([pizza, guac, berries])
+    end
+  end
 end
